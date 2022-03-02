f33bcd49de78886b82bad352c3bce0a3c48b4063
rohmatsyam <rohmatsyam@yahoo.com>
2022-03-02 21:08:24 +0700

    oke

--- latihan/tipe data.php
+++ latihan/tipe data.php
@@ -7,9 +7,55 @@
     <title>COBA</title>
 </head>
 <body>
-    <?php $arr=["Satu",1,"Oke"];?>
+    <?php 
+    $arr=["Satu",1,"Oke"];
+    $a = 10;
+    $b = 5;
+    ?>
     <h1>Aku adalah angka <?= $arr[0]; ?> ditulis <?= $arr[1]; ?> selesai <?= $arr[2]; ?></h1>
     <?= var_dump($arr) ?>
-    <hr>         
+    <hr>
+    <h1>OPERATOR</h1>
+    <hr>
+    <h2>Aritmatika</h2>
+    <?php $mod = $a % $b;
+    echo "$a % $b = $mod";    
+    echo "<br>";
+    $pow = $a ** $b;
+    echo "$a ** $b = $pow";    
+    ?>
+    <hr>
+    <h2>Penugasan</h2>
+    <?php 
+        $c = $a;
+        echo "c adalah $c";
+        echo "<br>";        
+        $c += $c;
+        echo "c ditambah c = $c";
+        echo "<br>";        
+        $c -= 5;
+        echo "c dikurangi 5 = $c";
+        echo "<br>";        
+        $c *= 5;
+        echo "c dikali 5 = $c";
+        echo "<br>";        
+        $c ** 2;
+        echo "c dipamgka 2 = $c";
+        echo "<br>";        
+        $c /= 10;
+        echo "c dibagi 10 = $c";
+        echo "<br>";        
+        $c %= 10;
+        echo "c modulus 10 = $c";        
+    ?>
+    <hr>
+    <h2>Penugasan</h2>
+    <?php $d=10;    
+    echo "D adalah $d";
+    $d++;
+    echo "increment = $d";
+    $d--;
+    echo "decrement = $d";
+    ?>    
 </body>
 </html>
